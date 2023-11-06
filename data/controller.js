4ee6f7de22a8dad079542a2a52c76ddecd41c79d
mymaster11tech <mymaster11tech@gmail.com>
2023-11-06 13:23:50 +0530

    m

--- data/controller.js
+++ data/controller.js
@@ -4,35 +4,6 @@ const jwt = require("jsonwebtoken");
 
 const config=require("../config/config")
 
-// let config = {
-//   server: 'AMAR\\AMARDUTT',
-//   // server: "192.168.1.43",
-//   // server: 'DESKTOP-9PUG3BB',
-//   // server:'erpsvr',
-//   // server:'DESKTOP-U57CKVJ',
-//   // server:'192.168.2.2',
-//   // server:"192.168.255.219",
-//   // server:"192.168.45.219",
-//   // sever:"",
-//   // server:"ERPSRV\\SQLEXPRESS",
-//   user:'sa',
-//   // password:'R00t@321',
-//   password:'amar',
-//   // password:"piyush",
-//   port:1433,
-//   database: 'Icsoft',
-//   // database: 'AEAPL',
-//   // database:'IcSoft',
-//   driver: 'msnodesqlv8',
-//   options: {
-//     trustedConnection: true,
-//     // encrypt:true,
-//     trustServerCertificate: true,
-//     // enableSNI: false,
-//     // secureOptions: require('constants').SSL_OP_NO_TLSv1_2,
-//   },
-// };
-
 const search = async (req, res) => {
   const querry = `Select c.CustCode, c.CustName,  e.Enq_Ref_No, e.Date_of_Enquiry, e.Due_Date, 
   p.InternalPartNo as Itemcode, sc.CategoryName as itemCategory, P.ProdName+'-'+ ep.ItemDescription as Item, g.Gradename, ep.Qty, emp.EmpName    
